ad8c4195c5c2b7efad928665a5f7d847fbf2b482
EserYueksel <eseryuksel@live.de>
2022-12-21 14:37:17 +0100

    update: - actionbarcolor changed to appbackground-color - feedback when rolled 6 "You are the lucky one!"

--- app/src/main/java/com/example/mycube/MainActivity.kt
+++ app/src/main/java/com/example/mycube/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,21 +13,25 @@ class MainActivity : AppCompatActivity() {
 
         val image = findViewById<ImageView>(R.id.cube)
         val button = findViewById<Button>(R.id.random_button)
+        val number6 = findViewById<TextView>(R.id.lucky6)
 
         // codeblock which runs on every click
         button.setOnClickListener {
             println("click")
             val random = (1..6).random()
-
+            number6.text = ""
+                    
             when (random) {
                 1 -> image.setImageResource(R.drawable.dice_1)
                 2 -> image.setImageResource(R.drawable.dice_2)
                 3 -> image.setImageResource(R.drawable.dice_3)
                 4 -> image.setImageResource(R.drawable.dice_4)
                 5 -> image.setImageResource(R.drawable.dice_5)
-                6 -> image.setImageResource(R.drawable.dice_6)
+                6 -> {image.setImageResource(R.drawable.dice_6)
+                     number6.text = "You are the lucky one!"}
+                }
             }
         }
-    }
 }
+
 
